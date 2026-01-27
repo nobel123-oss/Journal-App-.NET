01d9578ad31d0e625da124a940912f14c0b8d27a
Nobel Kumar Aryal <nobelnpc@gmail.com>
2026-01-27 23:18:43 +0545

    refactory

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/JournalApp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/JournalApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+014e9fab46941dd60e85b6048186e52479162a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JournalApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
